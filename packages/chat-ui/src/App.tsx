cc0e3d1493840ce06ca63d5c0c12f5c80dbdb16f
Luis Meneses <luismenesesep@gmail.com>
2025-02-27 09:49:45 -0500

    test commit

--- packages/chat-ui/src/App.tsx
+++ packages/chat-ui/src/App.tsx
@@ -2,16 +2,16 @@ import styles from './App.module.css';
 
 type MessageProps = {
   text: string;
-}
+};
 
 const Message = ({ text }: MessageProps) => {
-  return <h1>{text}</h1>
-}
+  return <h1>{text}</h1>;
+};
 
 const App = () => {
   return (
     <div className={styles.content}>
-      <Message text='Hi there!' />
+      <Message text="Hi there!" />
     </div>
   );
 };
